e72d959984cd964b42e0158af53d9672591cf640
Tati Machado <taaatimachado@gmail.com>
2020-07-03 22:32:54 -0300

    Atualização do código e interface

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 
-        //Faz requisição
-        let request = new XMLHttpRequest();
-        //Abre requisição
-        request.open("GET", "https://api.nasa.gov/planetary/apod?api_key=HxfsjMvsjh9PJ6dPJ40ikaqugjUIAJgQbjK8QVN3");
-        //Tratar eventos antes de enviar
-        request.onload = function(){
-            let apod = JSON.parse(request.responseText);
-            let videoAPOD = document.querySelector("#video");
-            let imgAPOD = document.querySelector("#image");
-            let dataAPOD = document.querySelector("#data");
-            let nomeImg = document.querySelector("#nomeImg");
-            let textoAPOD = document.querySelector("#textoImg");
-            let autorAPOD = document.querySelector("#autor");
+//Faz requisição
+    let request = new XMLHttpRequest();
+//Abre requisição
+    request.open("GET", `https://api.nasa.gov/planetary/apod?api_key=HxfsjMvsjh9PJ6dPJ40ikaqugjUIAJgQbjK8QVN3&date`);
+//Tratar eventos antes de enviar
+    request.onload = function(){
+        let apodObj = JSON.parse(request.responseText);
+        let imgAPOD = document.querySelector("#image");
+        let videoAPOD = document.querySelector("#video");
+        let dataAPOD = document.querySelector("#data");
+        let nomeImg = document.querySelector("#nomeImg");
+        let textoAPOD = document.querySelector("#textoImg");
+        let autorAPOD = document.querySelector("#autor");
 
-            textoAPOD.textContent = apod.explanation;
+            textoAPOD.textContent += apodObj.explanation;
+            dataAPOD.textContent = apodObj.date;
+            autorAPOD.textContent += apodObj.copyright;
+            nomeImg.textContent = apodObj.title;
+
             textoAPOD.classList.add("textoImg");
-            dataAPOD.textContent = apod.date;
             dataAPOD.classList.add("data");
-            autorAPOD.textContent += apod.copyright;
-            autorAPOD.classList.add("autor")
-            nomeImg.textContent = apod.title;
+            autorAPOD.classList.add("autor");
             nomeImg.classList.add("nomeImg");
 
 
-            if (typeof(imgAPOD == "img")) {
-                imgAPOD.src = apod.url;
-                imgAPOD.classList.add("image")
-                
-            } else {
-                videoAPOD.src = apod.url;
-                videoAPOD.classList.add("video")
+        if (imgAPOD.media_type = "image") {
+            imgAPOD.src = apodObj.url;
+            videoAPOD.classList.add("someclasse");
 
-            }
-        //Envia requisição
-        request.send();    
-        
+            
+                
+        } else {
+            videoAPOD.src = apodObj.url;
+            imgAPOD.classList.add("someclasse");
         }
-  
+         
+        videoAPOD.classList.add("video");
+        imgAPOD.classList.add("image");
+    }
+
+         //Envia requisição
+         request.send();  
